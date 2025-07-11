b201e8415f22cec2f50fb99e88731d0144f72cc5
TEDDY KIPCHIRCHIR <lonewalker634@gmail.com>
2025-07-11 16:43:58 +0300

    removed disease detection folder

--- disease_detection1/serializers.py
+++
@@ -1,6 +0,0 @@
-# disease_detection/serializers.py
-
-from rest_framework import serializers
-
-class DiseasePredictionSerializer(serializers.Serializer):
-    image = serializers.ImageField()
--- disease_detection1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import predict_disease 
-
-urlpatterns = [
-    path('predict/', predict_disease, name='predict-disease'),
-]
